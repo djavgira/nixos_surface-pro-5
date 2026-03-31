20712d9613fcf4d1af8dd84b260d935cdc7704fb
PainWind <a89831773ipo@outlook.com>
2026-03-31 15:40:00 +0800

    gc-1

--- modules/extra/gc.nix
+++ modules/extra/gc.nix
@@ -1,9 +1,12 @@
 { config, lib, pkgs, ... }:
 
 {
+  boot.loader.systemd-boot.configurationLimit = 10;
+
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 7d";
   };
+  nix.settings.auto-optimise-store = true;
 }
