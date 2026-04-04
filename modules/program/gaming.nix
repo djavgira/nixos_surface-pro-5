b5498116db4d428cabde5479838d3c260d1c713d
PainWind <a89831773ipo@outlook.com>
2026-04-04 08:09:40 +0800

    steam_lutrix_fix

--- modules/program/gaming.nix
+++ modules/program/gaming.nix
@@ -3,13 +3,13 @@
 {
   environment.systemPackages = with pkgs; [
     lutris
-    # steam
+    steam
   ];
 
-  # programs.steam = {
-  #   enable = true;
-  #   remotePlay.openFirewall = true;
-  #   dedicatedServer.openFirewall = true;
-  #   localNetworkGameTransfers.openFirewall = true;
-  # };
+   programs.steam = {
+     enable = true;
+     remotePlay.openFirewall = true;
+     dedicatedServer.openFirewall = true;
+     localNetworkGameTransfers.openFirewall = true;
+   };
 }
