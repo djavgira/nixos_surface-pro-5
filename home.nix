eb5ecd9e6951bbfc1066a39429859f12065b03a3
PainWind <a89831773ipo@outlook.com>
2026-06-15 22:48:51 +0800

    reduce zone

--- home.nix
+++ home.nix
@@ -31,7 +31,8 @@
   # 建议将所有 GUI 软件，以及与 OS 关系不大的 CLI 软件，都通过 home.packages 安装
   home.packages = with pkgs;[
     # 如下是我常用的一些命令行工具，你可以根据自己的需要进行增删
-    neofetch
+    pfetch
+    microfetch
     nnn # terminal file manager
 
     # archives
--- modules/program/minio-client.nix
+++ modules/program/minio-client.nix
@@ -8,7 +8,7 @@
     nix = {
     settings = {
       extra-substituters = [
-        "http://192.168.10.32:9000/nix-cache/"
+        "s3://nix-cache?region=guetsec-pve&endpoint=http://192.168.10.32:9000/nix-cache/"
       ];
       extra-trusted-public-keys = [
       	"s3.homelab.local-1:odBG/MiuZFzLwFV5+JZq7ArvIza5SgnY7aPrLQncx10="
