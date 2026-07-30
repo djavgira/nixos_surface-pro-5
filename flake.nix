0a08f1bdc484954204dc2e2ca1cbda2c11aea2a9
PainWind <a89831773ipo@outlook.com>
2026-07-30 19:19:07 +0800

    引入llm.nix仓库

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {
-	inherit llm-agents;
+	llm-agents = inputs.llm-agents;
       };
       modules = [
         ./nix/configuration.nix
