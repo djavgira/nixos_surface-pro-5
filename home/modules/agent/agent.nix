fc2a12ec4bc1e58b0156975d2d8dcdad778cca16
PainWind <a89831773ipo@outlook.com>
2026-07-30 16:32:19 +0800

    引入llm.nix仓库

--- home/modules/agent/agent.nix
+++ home/modules/agent/agent.nix
@@ -1,6 +1,10 @@
 { config, lib, pkgs, ...}:
 
 {
+  inputs = {
+    llm-agents.url = "github:numtide/llm-agents.nix";
+  };
+
   # In your system packages:
   environment.systemPackages = with inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; [
     pi
